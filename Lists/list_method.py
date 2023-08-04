b063fd1ba7aea0dba1bb3116afa869efc3a78f00
Md Sha Newaz <sawonnstu@gmail.com>
2023-08-05 01:20:02 +0600

    2nd

---
+++ Lists/list_method.py
@@ -0,0 +1,1 @@
+my_list = ["Soudia Arabia", "Iran", "Turkey", "Pakistan", "Indoneshia", "Egypt", "Albenia", "Bhutan", "Nepal", "France", "Italy"]
---
+++ Lists/slice2.py
@@ -0,0 +1,7 @@
+my_list = ["Soudia Arabia", "Iran", "Turkey", "Pakistan", "Indoneshia", "Egypt", "Albenia", "Bhutan", "Nepal", "France", "Italy"]
+
+new_list = my_list[1:5:2]
+print(new_list)
+
+new_list2 = my_list[-6:-1:2]
+print(new_list2)

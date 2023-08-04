357730097859f2b40b90105de5646e1400b528d4
Md Sha Newaz <sawonnstu@gmail.com>
2023-08-05 01:47:43 +0600

    last

--- Lists/list_method.py
+++ Lists/list_method.py
@@ -1,1 +1,23 @@
-my_list = ["Soudia Arabia", "Iran", "Turkey", "Pakistan", "Indoneshia", "Egypt", "Albenia", "Bhutan", "Nepal", "France", "Italy"]
+my_list = ["Soudia Arabia", "Iran", "Turkey", "Pakistan", "Indoneshia", "Egypt", "Albenia", "Bhutan", "Nepal", "France", "Italy", "France"]
+print(my_list)
+
+my_list.append("Bangladesh")
+print(my_list)
+
+my_list.append(["Maldiv", "Brunei"])
+print(my_list)
+
+my_list.extend(["Maldiv", "Brunei"])
+print(my_list)
+
+my_list.insert(-1, "Germany")
+print(my_list)
+
+my_list.remove("France")
+print(my_list)
+
+country_removed = my_list.pop(1)
+print(my_list)
+print(country_removed)
+
+print(my_list.clear())
---
+++ Lists/list_sort.py
@@ -0,0 +1,10 @@
+my_list = ["Soudia Arabia", "Iran", "Turkey", "Pakistan", "Indoneshia", "Egypt", "dubai", "Albenia", "Bhutan", "Nepal", "France", "Italy"]
+print(my_list)
+my_list.sort()
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+my_list.sort(key = str.upper, reverse=True)
+print(my_list)
